45ac8211fd07b3239a457f9630e01b41db9234ca
Chris Girvin <bugbounty.chrisg8691@gmail.com>
2021-06-30 06:05:50 -0400

    set font and big-font; added info to header

--- doom/config.el
+++ doom/config.el
@@ -20,6 +20,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14 :style "Heavy Oblique")
+      doom-big-font (font-spec :family "Iosevka Nerd Font" :size 24 :style "Heavy Oblique"))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
